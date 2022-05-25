3e5ddd507b4e44e81574eca56bb29147c8264e0f
anjalireghunath <anjalireghu98@gmail.com>
2022-05-26 01:58:27 +0530

    add bus

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import AddBus from './components/AddBus';
 function App() {
   return (
 <div>
-  
+<AddBus/>
 </div>
   );
 }
