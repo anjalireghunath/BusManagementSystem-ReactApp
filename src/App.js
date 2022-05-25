18c77b0678296b35821f8c6c2885edcb90e7d7ba
anjalireghunath <anjalireghu98@gmail.com>
2022-05-26 02:15:44 +0530

    search bus

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import AddBus from './components/AddBus';
+import SearchBus from './components/SearchBus';
 function App() {
   return (
 <div>
 <AddBus/>
+<SearchBus/>
 </div>
   );
 }
