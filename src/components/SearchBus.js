77e6a92fd93f51769405b496c66b52939a053b9c
anjalireghunath <anjalireghu98@gmail.com>
2022-05-28 00:34:53 +0530

    api delete

--- src/components/SearchBus.js
+++ src/components/SearchBus.js
@@ -1,12 +1,44 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 import Header from './Header'
 
 const SearchBus = () => {
+    const [data,setData]=useState([
+        {
+            "busname":"",
+            "owner":"",
+            "busregno":"",
+            "contactnum":""
+        }
+       
+    ])
+
     var [route,setRoute]=useState("")
-    const sData=()=>{
+    const searchData=()=>{
         const data={"route":route}
         console.log(data)
+        axios.post("http://localhost:4004/api/search",data).then((response)=>{
+            console.log(response.data)
+            setData(response.data)
+        })
     }
+
+    const deleteData=(id)=>{
+         const data={"_id":id}
+         console.log(data)
+         axios.post("http://localhost:4004/api/delete",data).then((response)=>{
+             if(response.data.status=="success")
+             {
+                 alert("successfully deleted")
+             }
+             else
+             {
+                 alert("failed to delete")
+             }
+         })
+    }
+
+
   return (
     <div>
 
@@ -20,12 +52,45 @@ const SearchBus = () => {
                     <input onChange={(a)=>{setRoute(a.target.value)}} type="text" className="form-control"/>
                 </div>   
                 <div className="col col-12 col-md-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <button onClick={sData} className="btn btn-primary">SEARCH</button>
+                    <button onClick={searchData} className="btn btn-primary">SEARCH</button>
                 </div>
             </div>
         </div>
     </div>
 </div>
+{data.map((value,key)=>{
+return <div className="container">
+    <div className="row">
+        <div className='col col-12 col-md-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12'>
+            <div className='row g-3'>
+                <div className="col col-12 col-md-6 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label for="" className="form-label">BusName</label>
+                <input type="text" className="form-control" value={value.busname}/>
+                </div>
+                <div className="col col-12 col-md-6 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label for="" className="form-label">BusRegNo</label>
+                <input type="text" className="form-control" value={value.busregno}/>
+                </div>
+                <div className="col col-12 col-md-6 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label for="" className="form-label">Owner</label>
+                <input type="text" className="form-control" value={value.owner}/>
+                </div>
+                <div className="col col-12 col-md-6 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label for="" className="form-label">ContactNum</label>
+                <input type="text" className="form-control" value={value.contactnum}/>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+    <div className='col col-12 col-md-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12'>
+<button onClick={()=>{deleteData(value._id)}} className='btn btn-danger'>DELETE</button>
+    </div>
+
+</div>
+})}
 
     </div>
   )
--- src/components/View.js
+++ src/components/View.js
@@ -4,10 +4,26 @@ import Header from './Header'
 
 const View = () => {
     var [viewbus,setViewbus]=useState([])
+    var [loadstatus,setLoadstatus]=useState(true)
     axios.get("http://localhost:4004/api/viewall").then((response)=>{
       console.log(response.data)
       setViewbus(response.data)
+      setLoadstatus(false)
     })
+    const deleteData=(id)=>{
+      const data={"_id":id}
+      console.log(data)
+      axios.post("http://localhost:4004/api/delete",data).then((response)=>{
+          if(response.data.status=="success")
+          {
+              alert("successfully deleted")
+          }
+          else
+          {
+              alert("failed to delete")
+          }
+      })
+ }
   return (
     <div>
 <Header/>
@@ -16,14 +32,18 @@ const View = () => {
             <div className='col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12'>
             <div className='row g-3'>
                 <div className='col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12'>
-                <table class="table table-primary table-striped">
-  <thead>
+{loadstatus ? <div class="spinner-border" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div> :
+<table class="table table-primary table-striped">
+  <thead className='table-danger'>
     <tr>
       <th scope="col">Route</th>
       <th scope="col">Bus Name</th>
       <th scope="col">BusRegNo</th>
       <th scope="col">OwnerName</th>
       <th scope="col">ContactNo</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
@@ -32,8 +52,9 @@ const View = () => {
       <td>{value.route}</td>
       <td>{value.busname}</td>
       <td>{value.busregno}</td>
-      <td>{value.ownername}</td>
+      <td>{value.owner}</td>
       <td>{value.contactnum}</td>
+      <button onClick={()=>{deleteData(value._id)}} className='btn btn-danger'>DELETE</button>
     </tr>
     })}
     
@@ -41,7 +62,9 @@ const View = () => {
     
   </tbody>
 </table>
+}
 
+                
 
 
                     </div>
